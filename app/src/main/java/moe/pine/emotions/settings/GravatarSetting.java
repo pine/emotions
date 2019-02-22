9f1016a211ab26e2e2b86ebd333214ece0262edf
Pine Mizune <pinemz@gmail.com>
2019-02-22 19:14:57 +0900

    feat: innocent

--- app/src/main/java/moe/pine/emotions/settings/GravatarSetting.java
+++ app/src/main/java/moe/pine/emotions/settings/GravatarSetting.java
@@ -3,8 +3,10 @@ package moe.pine.emotions.settings;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
+import javax.validation.constraints.NotNull;
+
 @Data
 @ConfigurationProperties(prefix = "gravatar")
 public class GravatarSetting {
-    private final String email;
+    private @NotNull String email;
 }
