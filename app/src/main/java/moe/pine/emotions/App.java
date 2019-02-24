89d0d513c2f2d005227de54c6e2a12313fb31332
Pine Mizune <pinemz@gmail.com>
2019-02-24 22:49:09 +0900

    feat: support heroku

--- app/src/main/java/moe/pine/emotions/App.java
+++ app/src/main/java/moe/pine/emotions/App.java
@@ -1,23 +1,23 @@
 package moe.pine.emotions;
 
 import moe.pine.emotions.properties.GravatarProperties;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
 @EnableConfigurationProperties(GravatarProperties.class)
-public class App extends SpringBootServletInitializer {
+public class App {
     public static void main(String[] args) {
-        SpringApplication.run(App.class, args);
-    }
+        String port = System.getenv("PORT");
+        if (StringUtils.isNotEmpty(port)) {
+            System.setProperty("server.port", port);
+        }
 
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(App.class);
+        SpringApplication.run(App.class, args);
     }
 }
