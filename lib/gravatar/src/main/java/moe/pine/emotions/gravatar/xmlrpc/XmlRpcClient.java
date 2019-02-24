ee0ad3ab4b27b338592b9f0e9231f60219102a0c
Pine Mizune <pinemz@gmail.com>
2019-02-25 00:31:55 +0900

    fix: use deepToString

--- lib/gravatar/src/main/java/moe/pine/emotions/gravatar/xmlrpc/XmlRpcClient.java
+++ lib/gravatar/src/main/java/moe/pine/emotions/gravatar/xmlrpc/XmlRpcClient.java
@@ -5,6 +5,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.xmlrpc.XmlRpcException;
 import org.apache.xmlrpc.client.XmlRpcClientConfig;
 
+import java.util.Arrays;
+
 @Slf4j
 public class XmlRpcClient {
     private final org.apache.xmlrpc.client.XmlRpcClient underlying =
@@ -15,7 +17,7 @@ public class XmlRpcClient {
     }
 
     public Object execute(String pMethodName, Object[] pParams) throws XmlRpcException {
-        log.debug(String.format("Execute XML-RPC :: method=%s, args=%s", pMethodName, pParams.toString()));
+        log.debug(String.format("Execute XML-RPC :: method=%s, args=%s", pMethodName, Arrays.deepToString(pParams)));
 
         final Object response = underlying.execute(pMethodName, pParams);
         log.debug(String.format("Executed XML-RPC :: method=%s, response=%s", pMethodName, response.toString()));
