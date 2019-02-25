386875f8b4525edc29758d252053a4b7f1d1cf55
Pine Mizune <pinemz@gmail.com>
2019-02-25 23:46:56 +0900

    feat: add ~/.netrc data

--- .circleci/setup-heroku.sh
+++ .circleci/setup-heroku.sh
@@ -3,3 +3,12 @@
 set -eu -o pipefail
 
 curl https://cli-assets.heroku.com/install.sh | sh
+
+cat >~/.netrc <<EOF
+machine api.heroku.com
+    login $HEROKU_EMAIL
+    password $HEROKU_API_TOKEN
+machine git.heroku.com
+    login $HEROKU_EMAIL
+    password $HEROKU_API_TOKEN
+EOF
