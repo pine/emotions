3d7ac67006874ee57fb003273a3c95289ec51791
Pine Mizune <pinemz@gmail.com>
2019-03-10 19:23:38 +0900

    fix: fix environment variable name

--- .circleci/setup-heroku.sh
+++ .circleci/setup-heroku.sh
@@ -7,8 +7,8 @@ curl https://cli-assets.heroku.com/install.sh | sh
 cat >~/.netrc <<EOF
 machine api.heroku.com
     login $HEROKU_EMAIL
-    password $HEROKU_API_TOKEN
+    password $HEROKU_API_KEY
 machine git.heroku.com
     login $HEROKU_EMAIL
-    password $HEROKU_API_TOKEN
+    password $HEROKU_API_KEY
 EOF
