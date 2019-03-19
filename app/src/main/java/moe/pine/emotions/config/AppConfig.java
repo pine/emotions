b2e687da0eecf6fcf27b195b204c6f208c06b052
Pine Mizune <pinemz@gmail.com>
2019-03-19 17:57:24 +0900

    fix: use custom task  executor

--- app/src/main/java/moe/pine/emotions/config/AppConfig.java
+++ app/src/main/java/moe/pine/emotions/config/AppConfig.java
@@ -3,6 +3,8 @@ package moe.pine.emotions.config;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 import java.util.Random;
 
@@ -13,4 +15,13 @@ public class AppConfig {
     public Random random() {
         return new Random();
     }
+
+    @Bean
+    public TaskExecutor taskExecutor() {
+        final ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setWaitForTasksToCompleteOnShutdown(true);
+        executor.setAwaitTerminationSeconds(60);
+        return executor;
+    }
 }
+
