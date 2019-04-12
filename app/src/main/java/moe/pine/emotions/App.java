8d0f5278cd009e4965bb5fc5cba2632538cee031
Pine Mizune <pinemz@gmail.com>
2019-04-12 23:30:53 +0900

    fix: remove unnecessary code

--- app/src/main/java/moe/pine/emotions/App.java
+++ app/src/main/java/moe/pine/emotions/App.java
@@ -1,6 +1,5 @@
 package moe.pine.emotions;
 
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -9,11 +8,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 public class App {
     public static void main(String[] args) {
-        final String port = System.getenv("PORT");
-        if (StringUtils.isNotEmpty(port)) {
-            System.setProperty("server.port", port);
-        }
-
         SpringApplication.run(App.class, args);
     }
 }
