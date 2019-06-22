5c786ed39c4db27f64f639858502d611c105894e
Pine Mizune <pinemz@gmail.com>
2019-06-22 18:41:59 +0900

    refactor: use `package-info.java`

--- app/src/main/java/moe/pine/emotions/properties/SlackProperties.java
+++ app/src/main/java/moe/pine/emotions/properties/SlackProperties.java
@@ -4,19 +4,24 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.validation.annotation.Validated;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.util.List;
 
 @Data
+@Validated
 @ConfigurationProperties("slack")
 public class SlackProperties {
-    private List<Workspace> workspaces;
+    private @NotNull List<Workspace> workspaces;
 
     @Data
+    @Validated
     @NoArgsConstructor
     @AllArgsConstructor
     public static class Workspace {
-        private String id;
-        private String token;
+        private @NotBlank String id;
+        private @NotBlank String token;
     }
 }
