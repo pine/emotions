9a5a3a47600b5e60dab2e6096b57276433452892
Pine Mizune <pinemz@gmail.com>
2019-06-22 19:07:03 +0900

    refactor: use `validation`

--- app/src/main/java/moe/pine/emotions/properties/GravatarProperties.java
+++ app/src/main/java/moe/pine/emotions/properties/GravatarProperties.java
@@ -2,15 +2,19 @@ package moe.pine.emotions.properties;
 
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.validation.annotation.Validated;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.util.List;
 
 
 @Data
+@Validated
 @ConfigurationProperties("gravatar")
 public class GravatarProperties {
-    private String email;
-    private String password;
-    private List<String> images;
-    private List<String> addresses;
+    private @NotBlank String email;
+    private @NotBlank String password;
+    private @NotNull List<String> images;
+    private @NotNull List<String> addresses;
 }
