30fde791a3f8b1cde8e9476f46857eb822b43f6d
Pine Mizune <pinemz@gmail.com>
2019-06-22 18:35:18 +0900

    refactor: use validation

--- app/src/main/java/moe/pine/emotions/properties/TwitterProperties.java
+++ app/src/main/java/moe/pine/emotions/properties/TwitterProperties.java
@@ -2,12 +2,16 @@ package moe.pine.emotions.properties;
 
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.validation.annotation.Validated;
+
+import javax.validation.constraints.NotBlank;
 
 @Data
+@Validated
 @ConfigurationProperties("twitter")
 public class TwitterProperties {
-    private String consumerKey;
-    private String consumerSecret;
-    private String accessToken;
-    private String accessTokenSecret;
+    private @NotBlank String consumerKey;
+    private @NotBlank String consumerSecret;
+    private @NotBlank String accessToken;
+    private @NotBlank String accessTokenSecret;
 }
