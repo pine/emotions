56f2712b018eaedaf3b2a56f9b3892d2ed312412
Pine Mizune <pinemz@gmail.com>
2020-02-29 04:48:34 +0900

    refactor: use ConstructorBinding

--- app/src/main/java/moe/pine/emotions/properties/TwitterProperties.java
+++ app/src/main/java/moe/pine/emotions/properties/TwitterProperties.java
@@ -1,21 +1,25 @@
 package moe.pine.emotions.properties;
 
-import lombok.Data;
+import lombok.Value;
+import lombok.experimental.NonFinal;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConstructorBinding;
 import org.springframework.validation.annotation.Validated;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import java.time.Duration;
 
-@Data
+@Value
+@NonFinal
 @Validated
+@ConstructorBinding
 @ConfigurationProperties("twitter")
 public class TwitterProperties {
-    private @NotBlank String consumerKey;
-    private @NotBlank String consumerSecret;
-    private @NotBlank String accessToken;
-    private @NotBlank String accessTokenSecret;
-    private @NotNull Duration connectionTimeout;
-    private @NotNull Duration readTimeout;
+    @NotBlank String consumerKey;
+    @NotBlank String consumerSecret;
+    @NotBlank String accessToken;
+    @NotBlank String accessTokenSecret;
+    @NotNull Duration connectionTimeout;
+    @NotNull Duration readTimeout;
 }
